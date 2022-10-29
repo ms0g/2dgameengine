c669c732766d0cde90441fac6ddb5761b44e2841
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-10-29 04:32:55 +0300

    fix(RenderColliderSystem): collider w/h times to transform scale x/y

--- src/Systems/RenderColliderSystem.hpp
+++ src/Systems/RenderColliderSystem.hpp
@@ -20,8 +20,8 @@ public:
             SDL_Rect colliderRect{
                     static_cast<int>(transform.position.x + collider.offset.x - camera.x),
                     static_cast<int>(transform.position.y + collider.offset.y - camera.y),
-                    collider.width,
-                    collider.height
+                    static_cast<int>(collider.width * transform.scale.x),
+                    static_cast<int>(collider.height * transform.scale.y)
             };
 
             SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);

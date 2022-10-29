3b168a6e505842f97851fa88039c970c02ed93c1
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-10-30 02:39:13 +0300

    feat(Components): added projectile component

---
+++ src/Components/ProjectileComponent.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+struct ProjectileComponent {
+    int duration;
+    int hitPercentDamage;
+    bool isFriendly;
+    int startTime;
+
+    explicit ProjectileComponent(
+            int duration = 10000,
+            int hitPercentDamage = 0,
+            bool isFriendly = false) {
+        this->duration = duration;
+        this->hitPercentDamage = hitPercentDamage;
+        this->isFriendly = isFriendly;
+        this->startTime = SDL_GetTicks();
+    }
+};

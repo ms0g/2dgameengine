f1fe0fab3f3b27d44cbc4eda1d1df3162e01f4ab
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-15 02:43:16 +0300

    feat(Systems): subscribed movement system to collision event

--- src/Systems/MovementSystem.hpp
+++ src/Systems/MovementSystem.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../ECS/ECS.h"
+#include "../EventBus/EventBus.hpp"
+#include "../Events/CollisionEvent.hpp"
 #include "../Components/TransformComponent.hpp"
 #include "../Components/RigidBodyComponent.hpp"
 
@@ -10,6 +12,42 @@ public:
     MovementSystem() {
         RequireComponent<TransformComponent>();
         RequireComponent<RigidBodyComponent>();
+    }
+
+    void SubscribeToEvents(const std::unique_ptr<EventBus>& eventBus) {
+        eventBus->SubscribeToEvent<MovementSystem, CollisionEvent>(this, &MovementSystem::OnCollision);
+    }
+
+    void OnCollision(CollisionEvent& event) {
+        Entity a = event.a;
+        Entity b = event.b;
+
+        Logger::Log("Collision event emitted: " + std::to_string(a.GetID()) + " and " + std::to_string(b.GetID()));
+
+        if (a.BelongsToGroup("enemies") && b.BelongsToGroup("obstacles")) {
+            OnEnemyHitsObstacle(a, b); // "a" is the enemy, "b" is the obstacle
+        }
+
+        if (a.BelongsToGroup("obstacles") && b.BelongsToGroup("enemies")) {
+            OnEnemyHitsObstacle(b, a); // "b" is the enemy, "a" is the obstacle
+        }
+    }
+
+    void OnEnemyHitsObstacle(Entity enemy, Entity obstacle) {
+        if (enemy.HasComponent<RigidBodyComponent>() && enemy.HasComponent<SpriteComponent>()) {
+            auto& rigidbody = enemy.GetComponent<RigidBodyComponent>();
+            auto& sprite = enemy.GetComponent<SpriteComponent>();
+
+            if (rigidbody.velocity.x != 0) {
+                rigidbody.velocity.x *= -1;
+                sprite.flip = (sprite.flip == SDL_FLIP_NONE) ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
+            }
+
+            if (rigidbody.velocity.y != 0) {
+                rigidbody.velocity.y *= -1;
+                sprite.flip = (sprite.flip == SDL_FLIP_NONE) ? SDL_FLIP_VERTICAL : SDL_FLIP_NONE;
+            }
+        }
     }
 
     void Update(double deltaTime, int mapWidth, int mapHeight) {

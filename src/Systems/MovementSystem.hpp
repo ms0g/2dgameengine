dd56652de74071da1068a9bba3849d7439932ade
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-15 02:55:20 +0300

    feat(Systems): prevented the player out of the window

--- src/Systems/MovementSystem.hpp
+++ src/Systems/MovementSystem.hpp
@@ -58,6 +58,18 @@ public:
             transform.position.x += rigidBody.velocity.x * deltaTime;
             transform.position.y += rigidBody.velocity.y * deltaTime;
 
+            // Prevent the main player from moving outside the map boundaries
+            if (entity.HasTag("player")) {
+                int paddingLeft = 10;
+                int paddingTop = 10;
+                int paddingRight = 50;
+                int paddingBottom = 50;
+                transform.position.x = transform.position.x < paddingLeft ? paddingLeft : transform.position.x;
+                transform.position.x = transform.position.x > mapWidth - paddingRight ? mapWidth - paddingRight : transform.position.x;
+                transform.position.y = transform.position.y < paddingTop ? paddingTop : transform.position.y;
+                transform.position.y = transform.position.y > mapHeight - paddingBottom ? mapHeight - paddingBottom : transform.position.y;
+            }
+
             if ((transform.position.x < 0 || transform.position.x > mapWidth ||
                  transform.position.y < 0 || transform.position.y > mapHeight) && !entity.HasTag("player")) {
                 entity.Kill();
